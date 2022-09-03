5eea302f36623611658168dff41cbd5c63de8c04
jameelSalhhadin <69424795+LoxductheGreat@users.noreply.github.com>
2022-09-03 14:04:46 -0400

    able to search for users and send messages to other users

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -38,4 +38,35 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { registerUser }
+const authUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body
+
+  const user = await User.findOne({ username })
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid username or password')
+  }
+})
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search ? {
+    $or: [
+      { username: { $regex: req.query.search, $options: 'i' } },
+      { email: { $regex: req.query.search, $options: 'i' } }
+    ]
+  } : {}
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } })
+  res.send(users)
+})
+
+module.exports = { registerUser, authUser, allUsers }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const { notFound, errorHandler } = require('./middleware/errormiddleware')
 
 const app = express()
 dotenv.config()
@@ -14,6 +16,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 2500
 
