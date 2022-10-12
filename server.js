5ae419080ff83993b1ce63f0972c48202b75baa3
jameelSalhhadin <69424795+LoxductheGreat@users.noreply.github.com>
2022-10-12 14:56:16 +0800

    checking ports for sockets

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const server = app.listen(PORT, console.log(`Server is running on Port ${PORT}`)
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000'
+    origin: process.env.SOCKETPORT
   }
 })
 
